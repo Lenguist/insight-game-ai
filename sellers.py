928bb64d2110b830fd130ff8ba822d01f9a68971
Lenguist <lenguist.mb@gmail.com>
2023-07-06 14:11:51 -0400

    twoseller

--- sellers.py
+++ sellers.py
@@ -42,7 +42,10 @@ class DescentArithmeticSeller(Seller):
   def make_offer(self):
     offer = self.state["last-offer"]-max(self.descent,1)
     self.descent = self.descent-1
-    return offer
+    if offer<self.value+1:
+      return self.value+1
+    else:
+      return offer
   def reset_state(self):
     super().reset_state()
     self.descent = self.init_descent
