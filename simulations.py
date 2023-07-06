40627fda7116b865d62c05df7e4999f27f3cb92b
firestar2202 <jhaddad2202@gmail.com>
2023-07-06 14:13:22 -0400

    added Q learning seller, & .ipynb creates graph

--- simulations.py
+++ simulations.py
@@ -6,7 +6,6 @@ import numpy as np
 # imp_incr constant, max_price random
 def basic_simulation(value, range_min, range_max, imp_incr, imp_init, rounds, seller, verbose=False):
   total_profit = 0
-  round_cnt = 0
   for i in range(rounds):
     maxprice = int(random.uniform(range_min, range_max+1))
     buyer = Buyer(maxprice, imp_init, imp_incr)
